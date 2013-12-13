4c7d33f291611928457505c265afa453a788e31d
Kamil Kieliszczyk <kamil@kieliszczyk.net>
2013-12-13 13:30:48 +0100

    Configure the Rspec to only accept the new syntax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,8 @@ $:.unshift File.join(GEM_ROOT, "lib")
 require 'fc-reminder'
 Dir[File.join(GEM_ROOT, "spec", "support", "**/*.rb")].each { |f| require f }
 
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
