f5b6bd4cbf0d79ddda73e491315cb683f01f44e2
Kamil Kieliszczyk <kamil@kieliszczyk.net>
2014-02-26 09:59:10 +0100

    Refactor tests for lib/fc-reminder.rb

--- spec/fc-reminder_spec.rb
+++ spec/fc-reminder_spec.rb
@@ -2,9 +2,7 @@ require 'spec_helper'
 
 describe FCReminder do
   context "#build" do
-    it "returns an instance of FCReminder::Reminder" do
-      expect(FCReminder.build).to be_instance_of(FCReminder::Reminder)
-    end
+    it { expect(FCReminder.build).to be_instance_of(FCReminder::Reminder) }
   end
 end
 
