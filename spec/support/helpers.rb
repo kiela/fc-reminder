6d7f8fa4178785f5150aa5b40e1d7760ca421fe8
Kamil Kieliszczyk <kamil@kieliszczyk.net>
2014-03-04 11:19:38 +0100

    Do not cross 80 characters per line

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -3,13 +3,13 @@ require 'fakeweb'
 FakeWeb.allow_net_connect = false
 
 def fake_page_without_match(url)
-  file = File.join(GEM_ROOT, "spec", "support", "fakeweb", "fake_page_without_match.html")
-  fake_html_page url, File.read(file)
+  path = [GEM_ROOT, "spec", "support", "fakeweb", "fake_page_without_match.html"]
+  fake_html_page(url, File.read(File.join(path)))
 end
 
 def fake_page_with_match(url)
-  file = File.join(GEM_ROOT, "spec", "support", "fakeweb", "fake_page_with_match.html")
-  fake_html_page url, File.read(file)
+  path = [GEM_ROOT, "spec", "support", "fakeweb", "fake_page_with_match.html"]
+  fake_html_page(url, File.read(File.join(path)))
 end
 
 def fake_html_page(url, body)
