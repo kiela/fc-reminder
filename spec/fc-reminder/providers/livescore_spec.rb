69be21d6a63a336bc0e0d507d0225f1abfc523ef
Kamil Kieliszczyk <kamil@kieliszczyk.net>
2014-03-08 23:34:32 +0100

    Refactor tests for FCReminder::Providers::LiveScore

--- spec/fc-reminder/providers/livescore_spec.rb
+++ spec/fc-reminder/providers/livescore_spec.rb
@@ -5,38 +5,42 @@ describe FCReminder::Providers::LiveScore do
   let(:team_name) { "Barcelona" }
 
   context "#initialize" do
-    it "should be instance of FCReminder::Providers::Base" do
-      expect(provider).to be_kind_of(FCReminder::Providers::Base)
-    end
+    it { expect(provider).to be_kind_of(FCReminder::Providers::Base) }
   end
 
   context "#url" do
-    it "returns valid url" do
+    it "should be valid" do
       expect(provider.url).to match(/^#{URI::regexp}$/)
     end
   end
 
   context "#run" do
-    context "there is no match that day" do
-      before { fake_page_without_match(provider.url) }
+    let(:default_value) { FCReminder::Providers::Base.new.run(team_name) }
 
-      it "returns default value" do
-        default_value = FCReminder::Providers::Base.new.run(team_name)
-        expect(provider.run(team_name)).to eq(default_value)
-      end
+    context "when there is no match that day" do
+      before { fake_page_without_match(provider.url) }
+      subject(:results) { provider.run(team_name) }
+
+      it { expect(results).to eq(default_value) }
     end
 
-    context "there is a match that day" do
+    context "when there is a match that day" do
       before { fake_page_with_match(provider.url) }
+      subject(:results) { provider.run(team_name) }
 
-      it "returns non-empty instance of Hash" do
-        expect(provider.run(team_name)).to be_an_instance_of(Hash)
-        expect(provider.run(team_name)).not_to be_empty
+      it { expect(results).not_to eq(default_value) }
+
+      context "has correct structure" do
+        %w(country league time team1 team2).each do |attr|
+          it { expect(results).to have_key(attr.to_sym) }
+        end
       end
 
-      %w(country league time team1 team2).each do |attr|
-        it "has return value which includes #{attr}" do
-          expect(provider.run(team_name)).to have_key(attr.to_sym)
+      context "has correct types" do
+        it { expect(results).to be_an_instance_of(Hash) }
+
+        %w(country league time team1 team2).each do |attr|
+          it { expect(results[attr.to_sym]).to be_an_instance_of String }
         end
       end
     end
