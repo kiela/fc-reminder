90e64b60e0c4c58fcf8883cade15bf24dbb55855
Kamil Kieliszczyk <kamil@kieliszczyk.net>
2014-03-10 21:27:00 +0100

    Refactor tests for FCReminder::Runner

--- spec/fc-reminder/runner_spec.rb
+++ spec/fc-reminder/runner_spec.rb
@@ -6,58 +6,55 @@ describe FCReminder::Runner do
   context "#initialize" do
     subject(:runner) { FCReminder::Runner.new(options) }
 
-    it "sets options" do
-      expect(runner.options).to eq(options)
-    end
-
-    it "initializes reminder" do
-      expect(runner.reminder).to be_an_instance_of(FCReminder::Reminder)
-    end
+    it { expect(runner.reminder).to be_kind_of(FCReminder::Reminder) }
+    it { expect(runner.options).to eq(options) }
   end
 
   context "setters" do
     subject(:runner) { FCReminder::Runner.new(options) }
 
-    it "disallows to change options after initializing" do
-      expect(runner).not_to respond_to(:options=)
+    context "#options=" do
+      it { expect(runner).not_to respond_to(:options=) }
     end
 
-    it "disallows to change reminder after initializing" do
-      expect(runner).not_to respond_to(:reminder=)
+    context "#reminder=" do
+      it { expect(runner).not_to respond_to(:reminder=) }
     end
   end
 
   context "#start" do
-    it "sets reminder" do
-      options = { daemon: false }
-      runner = FCReminder::Runner.new(options)
+    context "sets reminder" do
+      subject(:runner) { FCReminder::Runner.new({ daemon: false }) }
+      before do
+        allow(Clockwork).to receive(:every) { |&block| block.call }
+        allow(runner.reminder).to receive(:run)
+        allow(runner).to receive(:run)
+      end
 
-      allow(Clockwork).to receive(:every) { |&block| block.call }
-      allow(runner.reminder).to receive(:run)
-      allow(runner).to receive(:run)
-
-      expect(runner.reminder).to receive(:run)
-      runner.start
+      it do
+        expect(runner.reminder).to receive(:run)
+        runner.start
+      end
     end
 
-    it "starts normal process" do
-      options = { daemon: false }
-      runner = FCReminder::Runner.new(options)
+    context "starts normal process" do
+      subject(:runner) { FCReminder::Runner.new({ daemon: false }) }
+      before { allow(runner).to receive(:run) }
 
-      allow(runner).to receive(:run)
-
-      expect(runner).to receive(:run)
-      runner.start
+      it do
+        expect(runner).to receive(:run)
+        runner.start
+      end
     end
 
-    it "starts daemon process" do
-      options = { daemon: true }
-      runner = FCReminder::Runner.new(options)
+    context "starts daemon process" do
+      subject(:runner) { FCReminder::Runner.new({ daemon: true }) }
+      before { allow(runner).to receive(:daemonize) }
 
-      allow(runner).to receive(:daemonize)
-
-      expect(runner).to receive(:daemonize)
-      runner.start
+      it do
+        expect(runner).to receive(:daemonize)
+        runner.start
+      end
     end
   end
 end
