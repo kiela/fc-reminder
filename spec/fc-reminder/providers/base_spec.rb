cfcdfb664ec24b57a6da10158dfb54dd3172eaeb
Kamil Kieliszczyk <kamil@kieliszczyk.net>
2014-03-26 06:36:28 +0100

    Change descriptions in tests for FCReminder::Providers::Base

--- spec/fc-reminder/providers/base_spec.rb
+++ spec/fc-reminder/providers/base_spec.rb
@@ -4,21 +4,21 @@ describe FCReminder::Providers::Base do
   subject(:provider) { FCReminder::Providers::Base.new }
   let(:team_name) { "Barcelona" }
 
-  context "#initialize" do
-    context "client" do
-      it { expect(provider.client).to be_instance_of Mechanize }
+  describe "#initialize" do
+    it "sets #client" do
+      expect(provider.client).to be_instance_of Mechanize
     end
   end
 
-  context "'client' attribute" do
+  describe "#client" do
     it { expect(provider).not_to respond_to(:client=) }
   end
 
-  context "#url" do
+  describe "#url" do
     it { expect(provider.url).to be_empty }
   end
 
-  context "#run" do
+  describe "#run" do
     it { expect(provider.run(team_name)).to be_instance_of(Hash) }
     it { expect(provider.run(team_name)).to be_empty }
   end
