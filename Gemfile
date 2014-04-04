0f56eec1f53ef2d1bcedc73fe993a72682b11502
Kamil Kieliszczyk <kamil@kieliszczyk.net>
2014-04-04 12:57:48 +0200

    Try Test Coverage feature from CodeClimate

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ end
 group :test do
   gem 'rspec', '~> 3.0.0.beta'
   gem 'fakeweb', '~> 1.3.0'
+  gem 'codeclimate-test-reporter', '~> 0.3.0', require: false
 end
 
 platforms :rbx do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,11 @@
 GEM_ROOT = File.expand_path("../../", __FILE__)
 $:.unshift File.join(GEM_ROOT, "lib")
 
+if ENV["CODECLIMATE_REPO_TOKEN"]
+  require "codeclimate-test-reporter"
+  CodeClimate::TestReporter.start
+end
+
 require 'fc-reminder'
 Dir[File.join(GEM_ROOT, "spec", "support", "**/*.rb")].each { |f| require f }
 
